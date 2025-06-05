105b2b13ed19b2c9bb9786cdcd4388cbc08cd942
Benjamin Randolph <neb417@gmail.com>
2025-06-05 10:25:17 -0600

    Run migration.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                      :bigint           not null, primary key
+#  email_address           :string           not null
+#  first_name              :string
+#  last_name               :string
+#  password_digest         :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  current_organization_id :integer
+#
+# Indexes
+#
+#  index_users_on_email_address  (email_address) UNIQUE
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
