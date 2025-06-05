23bbad4fae0fa92960131eea411a3fbece4ecee1
Benjamin Randolph <neb417@gmail.com>
2025-06-05 13:39:33 -0600

    Update user to require first and last name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,6 @@ class User < ApplicationRecord
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
   validates :email_address, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
