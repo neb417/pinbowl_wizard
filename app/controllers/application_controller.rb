ebbabe631439b4b9bd9c624a49c4b7e4d64e0ec3
Benjamin Randolph <neb417@gmail.com>
2025-06-05 10:55:38 -0600

    Add some bells and whistles.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   include Authentication
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  private
+
+  def user_not_authorized
+    flash.now[:alert] = "You are not authorized to perform this action."
+    redirect_back_or_to(root_path)
+  end
 end
