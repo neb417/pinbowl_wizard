8ba6add85db6bc19652d7bb622aca89e52476c07
Benjamin Randolph <neb417@gmail.com>
2025-06-05 13:39:33 -0600

    Update user to require first and last name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,8 @@ RSpec.describe User, type: :model do
   it { is_expected.to have_many(:player_matches) }
 
   context "validations" do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:email_address) }
   end
 end
