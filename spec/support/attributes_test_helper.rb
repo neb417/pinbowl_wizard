d13a96ba3d2e60cf84789e5c3f5e69f523dff96b
Benjamin Randolph <neb417@gmail.com>
2025-06-06 14:55:11 -0600

    Add attributes for matches.

--- spec/support/attributes_test_helper.rb
+++ spec/support/attributes_test_helper.rb
@@ -10,4 +10,16 @@ module AttributesTestHelper
   def edit_machine_attributes
     { name: Faker::FunnyName.name }
   end
+
+  def valid_match_attributes
+    { round_id: create(:round).id, machine_id: create(:machine).id }
+  end
+
+  def invalid_match_attributes
+    { round_id: nil, machine_id: nil }
+  end
+
+  def edit_match_attributes
+    { round_id: create(:round).id, machine_id: create(:machine).id }
+  end
 end
