f43f3756d78bf728fa2a8f2c1d67c4a5ad02cba6
Benjamin Randolph <neb417@gmail.com>
2025-06-06 15:45:02 -0600

    Unskip seasons index spec.

--- spec/views/rounds/index.html.erb_spec.rb
+++ spec/views/rounds/index.html.erb_spec.rb
@@ -3,19 +3,12 @@ require 'rails_helper'
 RSpec.describe "rounds/index", type: :view do
   let(:season) { create(:season) }
   before(:each) do
-    assign(:rounds, [
-      Round.create!(
-        season: season
-      ),
-      Round.create!(
-        season: season
-      )
-    ])
+    assign(:rounds, create_list(:round, 2, season: season))
   end
 
-  xit "renders a list of rounds" do
+  it "renders a list of rounds" do
     render
     cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new("Season".to_s), count: 2
   end
 end
