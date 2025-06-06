e1622d3a2e478530dcb31cf42b99eaaf681f4b17
Benjamin Randolph <neb417@gmail.com>
2025-06-06 14:05:43 -0600

    Create a membership.

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -2,4 +2,24 @@ class MembershipsController < ApplicationController
   def new
     @membership = Membership.new
   end
+
+  def create
+    organization = Organization.find_by(code: membership_params[:organization_code])
+
+    if organization.present?
+      Membership.create(user: Current.user, organization: organization)
+      Current.user.add_role(:account, organization)
+      redirect_to dashboard_path, notice: "Joined organization!"
+    else
+      @membership = Membership.new
+      @membership.errors.add(:base, "Organization not found.")
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def membership_params
+    params.expect(membership: [ :organization_code ])
+  end
 end
--- spec/requests/memberships_spec.rb
+++ spec/requests/memberships_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Memberships", type: :request do
   let(:user) { create(:user) }
+  let(:org) { create(:organization) }
+  let(:valid_membership_params) { { organization_code: org.code } }
+  let(:invalid_membership_params) { { organization_code: "" } }
 
   before(:each) do
     sign_in_as(user)
@@ -11,6 +14,36 @@ RSpec.describe "Memberships", type: :request do
     it "renders successful response" do
       get new_membership_path
       expect(response).to be_successful
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a membership on successful POST" do
+      expect {
+      post memberships_path, params: { membership: valid_membership_params }
+      }.to change(Membership, :count).by(1)
+    end
+
+    it "renders successful response" do
+      post memberships_path, params: { membership: valid_membership_params }
+      expect(response).to redirect_to(dashboard_path)
+    end
+
+    it "adds an account for user" do
+      expect(user.roles).to eq([])
+      post memberships_path, params: { membership: valid_membership_params }
+      expect(org.accounts).to eq([ user ])
+    end
+
+    it "does not create a membership on unsuccessful POST" do
+      expect {
+      post memberships_path, params: { membership: invalid_membership_params }
+      }.to_not change(Membership, :count)
+    end
+
+    it "renders new with unprocessable response" do
+      post memberships_path, params: { membership: invalid_membership_params }
       expect(response).to render_template(:new)
     end
   end
