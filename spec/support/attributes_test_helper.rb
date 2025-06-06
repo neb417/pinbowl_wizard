0a39690587d58a50b678ffde745eaeefd2751e24
Benjamin Randolph <neb417@gmail.com>
2025-06-06 14:50:36 -0600

    Update params to attributes.

--- spec/support/attributes_test_helper.rb
+++ spec/support/attributes_test_helper.rb
@@ -1,9 +1,9 @@
-module ParamsTestHelper
-  def valid_machine_params
+module AttributesTestHelper
+  def valid_machine_attributes
     { name: Faker::FunnyName.name }
   end
 
-  def invalid_machine_params
+  def invalid_machine_attributes
     { name: "" }
   end
 
