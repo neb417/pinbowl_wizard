c836782601749d6041c3c4670b05fca0f2d6d1e4
Benjamin Randolph <neb417@gmail.com>
2025-06-06 14:57:43 -0600

    Make update test more robust.

--- spec/requests/matches_spec.rb
+++ spec/requests/matches_spec.rb
@@ -89,7 +89,11 @@ RSpec.describe "/matches", type: :request do
 
       it "updates the requested match" do
         match = Match.create! valid_attributes
+        expect(match.round_id).to_not eq(new_attributes[:round_id])
+        expect(match.machine_id).to_not eq(new_attributes[:machine_id])
+
         patch match_url(match), params: { match: new_attributes }
+
         match.reload
         expect(match.round_id).to eq(new_attributes[:round_id])
         expect(match.machine_id).to eq(new_attributes[:machine_id])
