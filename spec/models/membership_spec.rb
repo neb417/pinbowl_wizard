cb4e8b1b5e115d0d6b0a1c6d6ac9d8f40066208d
Benjamin Randolph <ben@benjamin-randolph.com>
2025-08-15 12:29:53 -0600

    Annotate memberships

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id              :bigint           not null, primary key
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organization_id :bigint
+#  user_id         :bigint
+#
+# Indexes
+#
+#  index_memberships_on_organization_id  (organization_id)
+#  index_memberships_on_user_id          (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
