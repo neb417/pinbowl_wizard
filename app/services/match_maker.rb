0958f3eb5b10c841943e337919fadbfba36d2f01
Benjamin Randolph <ben@benjamin-randolph.com>
2025-09-12 13:06:58 -0600

    Add player match hash for player matching.

--- app/services/match_maker.rb
+++ app/services/match_maker.rb
@@ -6,15 +6,34 @@ class MatchMaker
     self.season = round.season
     self.organization = season.organization
     self.machines = organization.machines
-    self.players = organization.accounts
+    self.accounts = organization.accounts
   end
 
   def call
+
+    matchings = match_hash(accounts.to_a)
+    binding.pry
+    # players.each do |player|
+    #   players.shift
+    #   matchings[player] = players
+    # end
     # Need to create matches between 2 players and 1 machine
     # No player should play the same player within the same round
     # No machine should host any of the same players
     # If there is an odd number of players,a 'bye' must be present
   end
 
-  private attr_accessor :round, :season, :organization, :machines, :players
+  private attr_accessor :round, :season, :organization, :machines, :accounts
+
+  def match_hash(players, matchings ={})
+    return matchings if players.empty? || players.count == 1
+
+    player = players.shift
+    matches = []
+    players.each do |p|
+      matches << p
+    end
+    matchings[player] = matches
+    match_hash(players, matchings)
+  end
 end
--- spec/services/match_maker_spec.rb
+++ spec/services/match_maker_spec.rb
@@ -5,15 +5,20 @@ RSpec.describe MatchMaker, type: :service do
 
   let(:organization) { create(:organization) }
   let(:season) { create(:season, organization:) }
-  let(:owner) { create(:owner_user, current_organization: organization) }
-  let(:player1) { create(:account_user, current_organization: organization) }
-  let(:player2) { create(:account_user, current_organization: organization) }
-  let(:player3) { create(:account_user, current_organization: organization) }
-  let(:player4) { create(:account_user, current_organization: organization) }
-  let(:player5) { create(:account_user, current_organization: organization) }
-  let(:player6) { create(:account_user, current_organization: organization) }
-  let(:machine1) { create(:machine, organization:) }
-  let(:machine2) { create(:machine, organization:) }
-  let(:machine3) { create(:machine, organization:) }
-  let(:machine4) { create(:machine, organization:) }
+  let(:round) { build(:round, season: season) }
+  let(:owner) { create(:owner_user, current_organization: organization, organization:) }
+  let!(:player1) { create(:account_user, current_organization: organization, organization:) }
+  let!(:player2) { create(:account_user, current_organization: organization, organization:) }
+  let!(:player3) { create(:account_user, current_organization: organization, organization:) }
+  let!(:player4) { create(:account_user, current_organization: organization, organization:) }
+  let!(:player5) { create(:account_user, current_organization: organization, organization:) }
+  let!(:player6) { create(:account_user, current_organization: organization, organization:) }
+  let!(:machine1) { create(:machine, organization:) }
+  let!(:machine2) { create(:machine, organization:) }
+  let!(:machine3) { create(:machine, organization:) }
+  let!(:machine4) { create(:machine, organization:) }
+
+  it 'does a thing' do
+    subject
+  end
 end
