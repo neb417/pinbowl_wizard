62fa8d32be921c2f2dfc9a41fa46144fb98897aa
Benjamin Randolph <ben@benjamin-randolph.com>
2025-09-13 12:45:20 -0600

    Build matches and player matches.

--- app/services/match_maker.rb
+++ app/services/match_maker.rb
@@ -10,8 +10,16 @@ class MatchMaker
   end
 
   def call
-
-    matchings = match_hash(accounts.to_a)
+    player_matching = build_player_matching(accounts.to_a)
+    player_matching.each do |player, matching|
+      build_matches(player, matching, machines.to_a)
+      # machine = match_machines.shift
+      # match = Match.create(machine: machine, round: round)
+      # PlayerMatch.create(match: match, user: player)
+      # matchings.each do |matching|
+      #   PlayerMatch.create(match: match, user: matching)
+      # end
+    end
     binding.pry
     # players.each do |player|
     #   players.shift
@@ -25,7 +33,7 @@ class MatchMaker
 
   private attr_accessor :round, :season, :organization, :machines, :accounts
 
-  def match_hash(players, matchings ={})
+  def build_player_matching(players, matchings ={})
     return matchings if players.empty? || players.count == 1
 
     player = players.shift
@@ -34,6 +42,18 @@ class MatchMaker
       matches << p
     end
     matchings[player] = matches
-    match_hash(players, matchings)
+    build_player_matching(players, matchings)
+  end
+
+  def build_matches(player, player_matching, machines)
+    return if  machines.empty? || player_matching.empty?
+
+    machine = machines.shift
+    player_match = player_matching.shift
+    match = Match.create(machine: machine, round: round)
+    PlayerMatch.create(match: match, user: player)
+    PlayerMatch.create(match: match, user: player_match)
+
+    build_matches(player, player_matching, machines)
   end
 end
