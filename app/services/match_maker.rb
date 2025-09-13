7674b5a504a89fa5e5b46488d3534a299bc627af
Benjamin Randolph <ben@benjamin-randolph.com>
2025-09-13 12:52:15 -0600

    Remove commented out code.

--- app/services/match_maker.rb
+++ app/services/match_maker.rb
@@ -13,22 +13,7 @@ class MatchMaker
     player_matching = build_player_matching(accounts.to_a)
     player_matching.each do |player, matching|
       build_matches(player, matching, machines.to_a)
-      # machine = match_machines.shift
-      # match = Match.create(machine: machine, round: round)
-      # PlayerMatch.create(match: match, user: player)
-      # matchings.each do |matching|
-      #   PlayerMatch.create(match: match, user: matching)
-      # end
     end
-    binding.pry
-    # players.each do |player|
-    #   players.shift
-    #   matchings[player] = players
-    # end
-    # Need to create matches between 2 players and 1 machine
-    # No player should play the same player within the same round
-    # No machine should host any of the same players
-    # If there is an odd number of players,a 'bye' must be present
   end
 
   private attr_accessor :round, :season, :organization, :machines, :accounts
